0ccfd215c4812ecf36c46b131102dd246281f152
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-06-12 09:15:30 +0000

    test 5

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,11 @@ const App = () => {
   return (
     <div>
         <Tooltip text="This is a tooltip" tag="h2">
-          <h2>Hover over me</h2>
+          <h2 className="tooltip"><div>Hover over me</div></h2>
         </Tooltip>
         <br />
         <Tooltip text="This is another tooltip" tag="p">
-          <p>Hover over me to see another tooltip</p>
+          <p className="tooltip "><div>Hover over me to see another tooltip</div></p>
         </Tooltip>
     </div>
   )
